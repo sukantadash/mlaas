409ce84f820f84c94300a1a35ff6ce171dd11b18
Sukanta Dash <sukanta.dash@gmail.com>
2025-06-29 08:55:56 -0700

    removed access key

--- script.sh
+++ script.sh
@@ -4,11 +4,11 @@
 pip install -r requirements.txt
 
 
-export KEYCLOAK_URL="https://keycloak-rh-sso.apps.cluster-qlzvh.qlzvh.sandbox205.opentlc.com/auth"
+export KEYCLOAK_URL="https://cluster.com/auth"
 export KEYCLOAK_REALM="maas"
 export KEYCLOAK_CLIENT_ID="3scale"
-export KEYCLOAK_CLIENT_SECRET="pYqs2K0ccRkHFfToKSfkOCKPJugfah0O"
-export THREESCALE_ADMIN_API_URL="https://maas-admin.apps.cluster-qlzvh.qlzvh.sandbox205.opentlc.com/admin/api/"
-export THREESCALE_ADMIN_API_KEY="c0fd0141f4772fb87d49a44c156fbfe9ddab5bb18ce3fcb968f2543e543f9a79"
+export KEYCLOAK_CLIENT_SECRET=""
+export THREESCALE_ADMIN_API_URL="https://cluster.com/admin/api/"
+export THREESCALE_ADMIN_API_KEY=""
 
 python mlaas.py
